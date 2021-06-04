10e444143e7199380ee77de537a30eba3ca7655e
emilia brasilio <ebrasilio@gmail.com>
2021-06-04 19:59:56 -0300

    finishing the game

--- js/snake/script.js
+++ js/snake/script.js
@@ -14,7 +14,7 @@ let food = {
 }
 
 function criarBG() {
-	context.fillStyle = "lightgreen";				// estilo: define a cor de fundo
+	context.fillStyle = "black";				// estilo: define a cor de fundo
 	context.fillRect(0, 0, 16 * box, 16 * box); // desenha a borda - (x, y, altura, largura)
 }
 
@@ -31,9 +31,6 @@ function drawFood(){
 	context.fillRect(food.x, food.y, box, box);
 }
 
-
-
-
 document.addEventListener('keydown', update);
 
 function update(event){
@@ -41,7 +38,6 @@ function update(event){
 	if(event.keyCode == 38 && direction != "down") direction = "up";
 	if(event.keyCode == 39 && direction != "left") direction = "right";
 	if(event.keyCode == 40 && direction != "up") direction = "down";
-
 }
 
 function iniciarJogo(){
@@ -50,6 +46,13 @@ function iniciarJogo(){
 	if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
 	if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
+	for(i = 1; i < snake.length; i++){
+		if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+			clearInterval(jogo);
+			alert('Game Over!');
+		}
+	}
+	
 	criarBG();
 	criarCobra();
 	drawFood();
@@ -62,8 +65,13 @@ function iniciarJogo(){
 	if(direction == "up") snakeY -= box;
 	if(direction == "down") snakeY += box;
 
-	snake.pop();
-
+	if(snakeX != food.x || snakeY != food.y){
+		snake.pop();
+	} else {
+		food.x = Math.floor(Math.random() * 15 + 1) * box;
+		food.y = Math.floor(Math.random() * 15 + 1) * box;
+	}
+	
 	let newHead = {
 		x: snakeX,
 		y: snakeY
